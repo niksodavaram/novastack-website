fe62976fa727681455bdb0f7bb8f48dbdf060500
Nik Sodavaram <niksodavaram@Niks-MacBook-Pro.local>
2025-09-10 23:06:44 +1000

    Fix wrangler.toml - remove unsupported build fields

--- novastack-website/src/app/about/page.tsx
+++ novastack-website/src/app/about/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { Container, Typography, Box } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import Logo from '../../components/logo';
 
 export default function AboutPage() {
   return (
     <Container maxWidth="md" sx={{ textAlign: 'center', py: 8 }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 4, alignItems: 'center' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '32px', alignItems: 'center' }}>
         {/* Logo Section */}
         <Logo />
 
         {/* About Us Section */}
-        <Box>
+        <div>
           <Typography variant="h4" gutterBottom>
             About Us
           </Typography>
@@ -19,10 +19,10 @@ export default function AboutPage() {
             NovaStack Technologies Pty Ltd is an Australian technology company
             dedicated to cutting-edge software and cloud solutions.
           </Typography>
-        </Box>
+        </div>
 
         {/* Company Registration Section */}
-        <Box>
+        <div>
           <Typography variant="h4" gutterBottom>
             Company Registration
           </Typography>
@@ -31,18 +31,18 @@ export default function AboutPage() {
           <Typography>Type: Proprietary limited by shares</Typography>
           <Typography>Registered In: Victoria, Australia</Typography>
           <Typography>Date of Registration: 9 September 2025</Typography>
-        </Box>
+        </div>
 
         {/* Contact Section */}
-        <Box>
-          <Typography variant="h4" gutterBottom sx={{ mt: 4 }}>
+        <div>
+          <Typography variant="h4" gutterBottom style={{ marginTop: '32px' }}>
             Contact
           </Typography>
           <Typography>
             Email: <a href="mailto:info@novastack.com.au">info@novastack.com.au</a>
           </Typography>
-        </Box>
-      </Box>
+        </div>
+      </div>
     </Container>
   );
 }
